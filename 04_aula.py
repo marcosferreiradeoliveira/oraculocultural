97cc924c5272af2d8ec4795fdd78ec9de8c85521
Marcos Ferreira <marcosferreira@MacBook-Air-de-Marcos.local>
2025-05-07 13:08:33 -0300

    primeiro commit

---
+++ 04_aula.py
@@ -0,0 +1,83 @@
+import streamlit as st
+from langchain.memory import ConversationBufferMemory
+
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+
+
+# OPENAI_API_KEY = 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
+
+TIPOS_ARQUIVOS_VALIDOS = [
+    'Site', 'Youtube', 'Pdf', 'Csv', 'Txt'
+]
+
+CONFIG_MODELOS = {'Groq': 
+                        {'modelos': ['llama-3.1-70b-versatile', 'gemma2-9b-it', 'mixtral-8x7b-32768'],
+                         'chat': ChatGroq},
+                  'OpenAI': 
+                        {'modelos': ['gpt-4o-mini', 'gpt-4o', 'o1-preview', 'o1-mini'],
+                         'chat': ChatOpenAI}}
+
+MEMORIA = ConversationBufferMemory()
+
+def carrega_modelo(provedor, modelo, api_key):
+    chat = CONFIG_MODELOS[provedor]['chat'](model=modelo, api_key=api_key)
+    st.session_state['chat'] = chat
+
+def pagina_chat():
+    st.header('🤖Bem-vindo ao Oráculo', divider=True)
+
+    chat_model = st.session_state.get('chat')
+    memoria = st.session_state.get('memoria', MEMORIA)
+    for mensagem in memoria.buffer_as_messages:
+        chat = st.chat_message(mensagem.type)
+        chat.markdown(mensagem.content)
+
+    input_usuario = st.chat_input('Fale com o oráculo')
+    if input_usuario:
+        chat = st.chat_message('human')
+        chat.markdown(input_usuario)
+
+        chat = st.chat_message('ai')
+        resposta = chat.write_stream(chat_model.stream(input_usuario))
+        
+        memoria.chat_memory.add_user_message(input_usuario)
+        memoria.chat_memory.add_ai_message(resposta)
+        st.session_state['memoria'] = memoria
+
+
+def sidebar():
+    tabs = st.tabs(['Upload de Arquivos', 'Seleção de Modelos'])
+    with tabs[0]:
+        tipo_arquivo = st.selectbox('Selecione o tipo de arquivo', TIPOS_ARQUIVOS_VALIDOS)
+        if tipo_arquivo == 'Site':
+            arquivo = st.text_input('Digite a url do site')
+        if tipo_arquivo == 'Youtube':
+            arquivo = st.text_input('Digite a url do vídeo')
+        if tipo_arquivo == 'Pdf':
+            arquivo = st.file_uploader('Faça o upload do arquivo pdf', type=['.pdf'])
+        if tipo_arquivo == 'Csv':
+            arquivo = st.file_uploader('Faça o upload do arquivo csv', type=['.csv'])
+        if tipo_arquivo == 'Txt':
+            arquivo = st.file_uploader('Faça o upload do arquivo txt', type=['.txt'])
+    with tabs[1]:
+        provedor = st.selectbox('Selecione o provedor dos modelo', CONFIG_MODELOS.keys())
+        modelo = st.selectbox('Selecione o modelo', CONFIG_MODELOS[provedor]['modelos'])
+        api_key = st.text_input(
+            f'Adicione a api key para o provedor {provedor}',
+            value=st.session_state.get(f'api_key_{provedor}'))
+
+        st.session_state[f'api_key_{provedor}'] = api_key
+    
+    if st.button('Inicializar Oráculo', use_container_width=True):
+        carrega_modelo(provedor, modelo, api_key)
+
+
+def main():
+    pagina_chat()
+    with st.sidebar:
+        sidebar()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 05_aula.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import (WebBaseLoader,
+                                                  YoutubeLoader, 
+                                                  CSVLoader, 
+                                                  PyPDFLoader, 
+                                                  TextLoader)
+
+def carrega_site(url):
+    loader = WebBaseLoader(url)
+    lista_documentos = loader.load()
+    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+    return documento
+
+def carrega_youtube(video_id):
+    loader = YoutubeLoader(video_id, add_video_info=False, language=['pt'])
+    lista_documentos = loader.load()
+    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+    return documento
+
+def carrega_csv(caminho):
+    loader = CSVLoader(caminho)
+    lista_documentos = loader.load()
+    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+    return documento
+
+def carrega_pdf(caminho):
+    loader = PyPDFLoader(caminho)
+    lista_documentos = loader.load()
+    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+    return documento
+
+def carrega_txt(caminho):
+    loader = TextLoader(caminho)
+    lista_documentos = loader.load()
+    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+    return documento
