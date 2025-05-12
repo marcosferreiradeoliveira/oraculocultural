6472bd3b659f42d2925c913bb5a4db2385517f46
Marcos Ferreira <marcosferreira@MacBook-Air-de-Marcos.local>
2025-05-12 12:38:06 -0300

    antes refatoracao

---
+++ utils/css.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def apply_custom_css():
+    st.markdown("""
+        <style>
+        /* CSS aqui (copie o conteúdo que já estava no app.py) */
+        </style>
+    """, unsafe_allow_html=True)
